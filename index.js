7eb4d4bdad86d3ec4cbdadfd11c58e7c116be0f6
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2021-10-18 21:34:23 +0100

    update linters

--- index.js
+++ index.js
@@ -1,50 +1,46 @@
-/* eslint-disable rule-you-want-to-disable */
-var books = [];
-const title = document.querySelector(".boot-title");
-const author = document.querySelector(".author");
-const btn = document.querySelector(".btn");
-const bookHolder = document.querySelector("#bookList");
+/* eslint-disable no-unused-vars */
+let books = [];
+const title = document.querySelector('.boot-title');
+const author = document.querySelector('.author');
+const btn = document.querySelector('.btn');
+const bookHolder = document.querySelector('#bookList');
 
-const addBook = function () {
-  let id = Math.random() 
-  let temp = books
-  console.log(temp)
-  
-  books.push({ id: id, title: title.value, author: author.value });
+function addBook() {
+  const id = Math.random();
+
+  books.push({ id, title: title.value, author: author.value });
   bookHolder.innerHTML += ` <span>${title.value}</span><br/>
   <span>${author.value}</span><br/>
   <button onclick="removeBook(${id})">Remove</button>
   <hr>`;
 
-  title.value = ""
-  author.value = ""
+  title.value = '';
+  author.value = '';
   // books = JSON.parse(localStorage.books)
-  localStorage.setItem('data',JSON.stringify(books))
-};
+  localStorage.setItem('data', JSON.stringify(books));
+}
 
-
-btn.addEventListener("click", addBook);
+btn.addEventListener('click', addBook);
 
 const removeBook = (id) => {
-  bookHolder.innerHTML = "";
+  bookHolder.innerHTML = '';
   books = books.filter((book) => book.id !== id);
-  console.log(books);
 
-  for (let x in books) {
+  for (let x = 0; x < books.length; x += 1) {
     bookHolder.innerHTML += ` <span>${books[x].title}</span><br/>
   <span>${books[x].author}</span><br/>
   <button onclick="removeBook(${books[x].id})">Remove</button>
   <hr>`;
   }
-  localStorage.setItem('data',JSON.stringify(books))
+  localStorage.setItem('data', JSON.stringify(books));
 };
-window.onload = function (){
-  books = JSON.parse(localStorage.getItem('data'))
-  
-  for (let x in books) {
+window.onload = function onload() {
+  books = JSON.parse(localStorage.getItem('data'));
+
+  for (let x = 0; x < books.length; x += 1) {
     bookHolder.innerHTML += ` <span>${books[x].title}</span><br/>
   <span>${books[x].author}</span><br/>
   <button onclick="removeBook(${books[x].id})">Remove</button>
   <hr>`;
   }
-}
+};
