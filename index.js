9a4c6b268e669a1339a1c4cad3a18e4e14794335
Rafael Echart <rafaelechartsaltos@gmail.com>
2021-10-18 13:12:03 -0500

    Remove data function and DOM refrsh

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
-const title = document.querySelector('.boot-title')
-const author = document.querySelector('.author')
-const btn = document.querySelector('.btn')
-const bookHolder = document.querySelector('#bookList')
+/* eslint-disable rule-you-want-to-disable */
+const title = document.querySelector(".boot-title");
+const author = document.querySelector(".author");
+const btn = document.querySelector(".btn");
+const bookHolder = document.querySelector("#bookList");
 
 let books = [];
 const addBook = function () {
-  books.push({title: title.value,author:author.value})
+  let generated_ID = Math.random();
+
+  books.push({ id: generated_ID, title: title.value, author: author.value });
   console.log(books);
   bookHolder.innerHTML += ` <span>${title.value}</span><br/>
   <span>${author.value}</span><br/>
-  <button>Remove</button>
-  <hr>`
-}
+  <button onclick="removeBook(${generated_ID})">Remove</button>
+  <hr>`;
 
-btn.addEventListener('click', addBook)
+  title.value = ""
+  author.value = ""
+};
+
+btn.addEventListener("click", addBook);
+
+const removeBook = (id) => {
+  bookHolder.innerHTML = "";
+  books = books.filter((book) => book.id !== id);
+  console.log(books);
+
+  for (let x in books) {
+    bookHolder.innerHTML += ` <span>${books[x].title}</span><br/>
+  <span>${books[x].author}</span><br/>
+  <button onclick="removeBook(${books[x].id})">Remove</button>
+  <hr>`;
+  }
+};
